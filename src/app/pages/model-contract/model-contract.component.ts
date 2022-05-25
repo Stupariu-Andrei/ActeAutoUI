97dab5fae353ccc5940309278c8593b92e747087
Stupariu-Andrei <stupariuandrei@yahoo.com>
2022-05-25 15:19:59 +0300

    Resolving nav redirects.

--- src/app/pages/model-contract/model-contract.component.ts
+++ src/app/pages/model-contract/model-contract.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModelContractComponent implements OnInit {
 
-  pdfSrc = "../../../assets/Contract.pdf";
+  pdfSrc = "../../../assets/model_contract.pdf";
 
   constructor() { }
 
